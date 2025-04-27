7c01719dd326a48c7bf6c8cc17834e16bf571b57
MoustafaAmer12 <moustafaesam20113@gmail.com>
2025-04-28 02:49:48 +0300

    Dataset Loader change name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from dataset import preparation
+from dataset import loader
 
 def main():
-    X_train, X_test, y_train, y_test =  preparation.load_split_dataset()
+    X_train, X_test, y_train, y_test =  loader.load_split_dataset()
     print("Training set shape:", X_train.shape)
     print("Testing set shape:", X_test.shape)
     print("Training labels shape:", y_train.shape)
     print("Testing labels shape:", y_test.shape)
-    
+
 if __name__ == "__main__":
     main()
