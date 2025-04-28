dce2b4b66c5e8f85f579eeff9789f9204c9a9b54
Aly El Din <eng.alyeldin@gmail.com>
2025-04-28 04:18:58 +0300

    Testing still not works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataset import loader
+from Clustering.GMM.clustering_GMM import GMM
 
 def main():
     X_train, X_test, y_train, y_test =  loader.load_split_dataset()
@@ -6,6 +7,13 @@ def main():
     print("Testing set shape:", X_test.shape)
     print("Training labels shape:", y_train.shape)
     print("Testing labels shape:", y_test.shape)
+    number_of_clusters = [20,40,60]
+    #GMM
+    for K in number_of_clusters:
+        gmm = GMM(n_components = K)
+        gmm.fit(X_train)
+        train_labels_pred = gmm.predict(X_train)
+        print(f"Trained labels for k={K}")
 
 if __name__ == "__main__":
     main()
